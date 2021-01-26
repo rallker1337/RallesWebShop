ff69cd47a0abea699b20bff88ec49bc038864b64
rallker1337 <rass1994@gmail.com>
2021-01-26 21:34:10 +0100

    Add project files.

---
+++ BootstrapTest/Controllers/PrivacyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BootstrapTest.Models;
+
+namespace BootstrapTest.Controllers
+{
+	public class PrivacyController : Controller
+	{
+		private readonly ILogger<PrivacyController> _logger;
+
+		public PrivacyController(ILogger<PrivacyController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
